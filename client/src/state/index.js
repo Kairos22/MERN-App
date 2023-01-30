01549057891953a2632e249a71895060eed0594f
Kairos22 <alandiko84@gmail.com>
2023-01-30 17:50:37 +0100

    React file folder architecture

---
+++ client/src/state/index.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  mode: "light",
+  user: null,
+  token: null,
+  posts: [],
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+  },
+});
